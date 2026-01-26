38bf99c42b18e18fb34cd99bb365c5811711eb49
Kenton Troy <kentontroy@yahoo.com>
2026-01-25 23:03:44 -0500

    Delete test.sql

--- test.sql
+++
@@ -1,9 +0,0 @@
-TRUNCATE TABLE dbo.cdc_multiset_state;
-
-UPDATE dbo.cdc_multiset_watermark
-SET last_lsn = sys.fn_cdc_get_min_lsn(N'SalesLT_Product'),
-    last_seq = 0x00000000000000000000,
-    last_op  = 0
-WHERE capture_instance = N'SalesLT_Product';
-
-GO
