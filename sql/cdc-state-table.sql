8ee9b2858ddf9769c4dfcb1171d8c0ebfae65689
Kenton Troy <kentontroy@yahoo.com>
2026-01-27 11:40:41 -0500

    Modify cdc_multiset_state table structure
    Added rowversion and is_deleted column to cdc_multiset_state table.

--- sql/cdc-state-table.sql
+++ sql/cdc-state-table.sql
@@ -1,15 +1,3 @@
-/*
-ALTER TABLE dbo.cdc_multiset_state
-ADD state_rv rowversion;
-
-Existing rows get a row version value automatically
-Future inserts/updates will bump it automatically
-
-ALTER TABLE dbo.cdc_multiset_state
-ADD is_deleted bit NOT NULL
-    CONSTRAINT DF_cdc_multiset_state_is_deleted DEFAULT (0);
-*/
-
 CREATE TABLE dbo.cdc_multiset_state (
   capture_instance sysname        NOT NULL,
   row_sig          varbinary(32)  NOT NULL,   -- SHA2_256
@@ -19,6 +7,20 @@ CREATE TABLE dbo.cdc_multiset_state (
   last_seq         binary(10)     NULL,
   CONSTRAINT PK_cdc_multiset_state PRIMARY KEY (capture_instance, row_sig)
 );
+
+ALTER TABLE dbo.cdc_multiset_state
+ADD state_rv rowversion;
+
+ALTER TABLE dbo.cdc_multiset_state
+ADD state_rv_bigint AS CONVERT(bigint, state_rv) PERSISTED;
+
+CREATE INDEX IX_state_capture_rvbigint
+ON dbo.cdc_multiset_state (capture_instance, state_rv_bigint);
+
+ALTER TABLE dbo.cdc_multiset_state
+ADD is_deleted bit NOT NULL
+CONSTRAINT DF_cdc_multiset_state_is_deleted DEFAULT (0);
+
 
 CREATE TABLE dbo.cdc_multiset_watermark (
   capture_instance sysname    NOT NULL CONSTRAINT PK_cdc_multiset_watermark PRIMARY KEY,
